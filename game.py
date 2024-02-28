0fc249969dc42497e126b6dde6e33e997181c1c9
Brian Kimathi <bryo.kim1@gmail.com>
2024-02-28 17:18:46 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,100 @@
+import random
+
+from gameboard import GameBoard
+from player import Player
+
+
+def game_loop(game_board: GameBoard, player_X: Player, player_O: Player):
+    """Game logic.
+
+    Args:
+        game_board (GameBoard): Board for the game.
+        player_X (Player): First player.
+        player_O (Player): Second Player.
+    """
+    leave_loop = False
+    check = False
+    turn_count = 0
+
+    while leave_loop is False:
+        # player_X's turn
+        if turn_count % 2 == 0:
+            game_board.print_board()
+
+            while True:
+                try:
+                    number_picked = int(
+                        input(f"\nChoose a number {game_board.choices}: ")
+                    )
+                except ValueError:
+                    print("You must pick a number. Please try again")
+                else:
+                    if number_picked in game_board.choices:
+                        game_board.modify_board(number_picked, player_X.symbol)
+                        player_X.update_positions(number_picked)
+
+                        # Remove any combination with number picked from other player's
+                        # possible winning combinations
+                        player_O.filter_winning_combinations(number_picked)
+
+                        turn_count += 1
+
+                        if check and player_X.check_win():
+                            game_board.print_board()
+                            print(f"\n{player_X.symbol} Won")
+                            leave_loop = True
+
+                        break
+                    else:
+                        print("Invalid choice. Please try again")
+
+        # player_O's turn - Computer
+        else:
+            number_picked = random.choice(game_board.choices)
+            print(f"\nComputer choice: {number_picked}")
+
+            if number_picked in game_board.choices:
+                game_board.modify_board(number_picked, player_O.symbol)
+                player_O.update_positions(number_picked)
+
+                player_X.filter_winning_combinations(number_picked)
+
+                turn_count += 1
+
+                if check and player_O.check_win():
+                    game_board.print_board()
+                    print(f"\n{player_O.symbol} Won")
+                    leave_loop = True
+
+        if turn_count % game_board.rows:
+            check = True
+
+        if (len(game_board.choices) == 0 and leave_loop is False) or (
+            len(player_X.winning_combinations) == 0
+            and len(player_O.winning_combinations) == 0
+        ):
+            print("\nDraw")
+            leave_loop = True
+
+
+def main():
+    """Entry point"""
+    try:
+        size = int(input("Enter the size of the board (Defaults to 3): "))
+    except ValueError:
+        print("Invalid value for size. Default used.")
+        size = 3
+    else:
+        if size < 3:
+            print("Size cannot be less than 3. Using default")
+            size = 3
+
+    game_board = GameBoard(size, size)
+    player_X = Player("X", game_board.combinations)
+    player_O = Player("O", game_board.combinations)
+
+    game_loop(game_board, player_X, player_O)
+
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,58 @@
+"""Implementation of the Player class"""
+
+class Player:
+    """A player in the tic tac toe game."""
+
+    def __init__(
+        self, symbol: str, winning_combinations: list[list[int]] = []
+    ) -> None:
+        """Initialize a new player.
+
+        Args:
+            symbol (str): Symbol to represent the player.
+            winning_combinations (list[list[int]], optional): List of all the
+                combinations the player can win by. Defaults to [].
+        """
+        # Either 'X' or 'O' or any other symbol a player chooses.
+        self.symbol = symbol
+
+        # List of all combinations that the player can win by
+        self.winning_combinations = winning_combinations
+
+        # Set of the positions a player has picked.
+        self.positions: set[int] = set()
+
+    def update_positions(self, new_position: int) -> None:
+        """Update the positions a player has picked.
+
+        Args:
+            new_position (int): New position that the player has picked.
+        """
+        self.positions.add(new_position - 1)
+
+    def filter_winning_combinations(self, choice: int) -> None:
+        """Filter winning combinations after the other player has played.
+        Removes any combinations that have the choice selected by other player.
+
+        Args:
+            choice (int): Position selected by other player.
+        """
+        choice -= 1
+
+        self.winning_combinations = list(
+            filter(lambda x: choice not in x, self.winning_combinations)
+        )
+
+    def check_win(self) -> bool:
+        """Check if player has won.
+        Checks to see if a winning combination is among the player's picked
+        positions.
+
+        Returns:
+            bool: True if player has won, otherwise False.
+        """
+        for combination in self.winning_combinations:
+            if not set(combination).difference(self.positions):
+                return True
+
+        return False
